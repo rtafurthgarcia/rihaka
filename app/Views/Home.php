4b3471d74bc3a5a60b16df475012a0c9b2448c7d
Richard Tafurth-Garcia <r.tafurthgarcia@hftm.ch>
2022-09-15 18:28:01 +0000

    Basic punchline lol

--- app/Views/Home.php
+++ app/Views/Home.php
@@ -1,1 +1,7 @@
-<h1>Stuff's gonna be spicy</h1>
+<div class="container-fluid text-center bg-light">
+    <div class="col-md-5 p-lg-5 mx-auto my-5">
+        <h1 class="display-4 fw-normal">Kinkshame your local scriptkiddie</h1>
+        <p class="lead fw-normal">Im totally out of inspiration not gonna lie.</p>
+        <a class="btn btn-outline-secondary" href="#">Help im bad at webdev</a>
+    </div>
+</div>
