40d879d74bb034636b2e0a67c449e6e13e9b68db
Richard Tafurth-Garcia <r.tafurthgarcia@hftm.ch>
2022-09-15 18:20:46 +0000

    Added a basic footer

---
+++ app/Views/Base/Footer.php
@@ -0,0 +1,12 @@
+<div class="container">
+  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-6 border-top">
+    <p class="col-md-6 mb-0 text-muted">2022 RIHAKA, a lovely school project from the HFTM, for the HTFM</p>
+
+    <ul class="nav col-md-4 justify-content-end">
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Contact</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Impressum</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Privacy policy</a></li>
+      <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">FAQs</a></li>
+    </ul>
+  </footer>
+</div>
--- app/Views/Base/Layout.php
+++ app/Views/Base/Layout.php
@@ -9,6 +9,7 @@
   <body>
     <?=$this->fetch('./Base/Header.php', [])?>
     <?=$content?>
+    <?=$this->fetch('./Base/Footer.php', [])?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
   </body>
 </html>
