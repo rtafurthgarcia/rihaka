bc04b77e7c5494fd1af80dd7f9ed888c3dbedb2b
Richard Tafurth-Garcia <r.tafurthgarcia@hftm.ch>
2022-09-17 17:28:32 +0000

    Basic paragraphs

--- app/Views/Home.php
+++ app/Views/Home.php
@@ -4,6 +4,7 @@
             <div class="col-12 col-lg-6 pb-4">
                 <h1 class="display-4 fw-normal">Kinkshame your local scriptkiddie</h1>
                 <p class="lead fw-normal">Why fear them when you can laugh at them?</p>
+                <p class="lead fw-normal">Or maybe actually they can teach you something valuable?</p>
                 <a class="btn btn-outline-secondary" href="#">Plz I wanna setup my own honeypot now</a>
             </div>
             <div class="col-12 col-lg-6 container shadow p-0 m-0 pb-5">
@@ -19,6 +20,39 @@
         </div>
     </div>
 </div>
+
+<div class="container px-4 py-5" id="hanging-icons">
+    <h2 class="pb-2 border-bottom">Don't let your honeypot logs go to waste no more</h2>
+    <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
+      <div class="col d-flex align-items-start">
+        <div class="text-dark flex-shrink-0 me-3 fs-2 mb-3">
+            <i class="icon-square bi bi-camera-video-fill"></i>
+        </div>
+        <div>
+          <h2>Record 'em</h2>
+          <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
+        </div>
+      </div>
+      <div class="col d-flex align-items-start">
+        <div class="text-dark flex-shrink-0 me-3 fs-2 mb-3">
+            <i class="icon-square bi bi bi-share-fill"></i>
+        </div>
+        <div>
+          <h2>Share 'em</h2>
+          <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
+        </div>
+      </div>
+      <div class="col d-flex align-items-start">
+        <div class="text-dark flex-shrink-0 me-3 fs-2 mb-3">
+            <i class="icon-square bi bi-mortarboard-fill"></i>
+        </div>
+        <div>
+          <h2>Learn from 'em?</h2>
+          <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
+        </div>
+      </div>
+    </div>
+  </div>
 
 <script src="/js/asciinema-player.min.js"></script>
 <script>
