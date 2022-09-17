e0e51a25ebe03b2ec92ec2ec427a7a7bc1cff469
Richard Tafurth-Garcia <r.tafurthgarcia@hftm.ch>
2022-09-17 17:08:20 +0000

    Fixed port.  Fixed sized for demo terminal -> shows something okayish style-wise.

--- app/Views/Base/Layout.php
+++ app/Views/Base/Layout.php
@@ -5,7 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?=$page_title?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.9.1/font/bootstrap-icons.css">
     <link rel="stylesheet" type="text/css" href="/css/asciinema-player.css" />
+    <link rel="stylesheet" type="text/css" href="/css/style.css" />
   </head>
   <body>
     <?=$this->fetch('./Base/Header.php', [])?>
--- app/Views/Home.php
+++ app/Views/Home.php
@@ -1,12 +1,21 @@
 <div class="container-fluid text-center bg-dark text-light">
     <div class="container">
-        <div class="d-flex pb-4">
-            <div>
+        <div class="row">
+            <div class="col-12 col-lg-6 pb-4">
                 <h1 class="display-4 fw-normal">Kinkshame your local scriptkiddie</h1>
                 <p class="lead fw-normal">Why fear them when you can laugh at them?</p>
                 <a class="btn btn-outline-secondary" href="#">Plz I wanna setup my own honeypot now</a>
             </div>
-            <div class="img-fluid w-50 shadow rounded-3" id="demo"></div>
+            <div class="col-12 col-lg-6 container shadow p-0 m-0 pb-5">
+                <div class="console-header row d-flex justify-content-end align-items-center">
+                    <i class="bi bi-dash-lg col-1"></i>
+                    <i class="bi bi-square col-1"></i>
+                    <i class="bi bi-x-lg col-1"></i>
+                </div>
+                <div class="row">
+                    <div class="col p-0 m-0" id="demo"></div>
+                </div>
+            </div>
         </div>
     </div>
 </div>
@@ -16,8 +25,7 @@
     AsciinemaPlayer.create('/videos/demo.cast', document.getElementById('demo'), {
         autoPlay: true, 
         loop: true,
-        preload: true,
-        terminalFontSize: "12px",
-        fit: "none"
+        speed: 2,
+        fit: "height"
     });
 </script>
