443ddd56ec3fa2b5290308f84c4a249b5dbb156a
Richard Tafurth-Garcia <r.tafurthgarcia@hftm.ch>
2022-09-17 22:26:48 +0000

    Changed header

--- app/Views/Base/Header.php
+++ app/Views/Base/Header.php
@@ -1,19 +1,19 @@
 <header class="p-3 text-bg-dark">
     <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
       <div class="container">
-        <a href="#" class="navbar-brand">RIHAKA</a>
+        <a href="#" class="navbar-brand fs-2">RIHAKA</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navmenu">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse">
-          <ul class="navbar-nav ms-auto" id="navmenu">
+          <ul class="navbar-nav" id="navmenu">
             <li class="nav-item"><a class="nav-item nav-link active" href="#" aria-current="page">Home <span class="visually-hidden">(current)</span></a></li>
             <li class="nav-item"><a href="#" class="nav-link px-2 text-white">Categories</a></li>
             <li class="nav-item"><a href="#" class="nav-link px-2 text-white">Explore</a></li>
             <li class="nav-item"><a href="#" class="nav-link px-2 text-white">Setup</a></li>
             <li class="nav-item"><a href="#" class="nav-link px-2 text-white">Community</a></li>
           </ul>
-          <div class="text-end">
+          <div class="ms-auto text-end">
             <button type="button" class="btn btn-outline-light me-2">Login</button>
             <button type="button" class="btn btn-warning">Sign up</button>
           </div>
--- app/Views/Home.php
+++ app/Views/Home.php
@@ -7,8 +7,8 @@
                 <p class="lead fw-normal">Or maybe actually they can teach you something valuable?</p>
                 <a class="btn btn-outline-secondary" href="#">Plz I wanna setup my own honeypot now</a>
             </div>
-            <div class="col-12 col-lg-6 container shadow p-0 m-0 pb-5">
-                <div class="console-header row d-flex justify-content-end align-items-center">
+            <div class="col-12 col-lg-5 container p-0 m-0 pb-5">
+                <div class="console-header row justify-content-end align-items-center">
                     <i class="bi bi-dash-lg col-1"></i>
                     <i class="bi bi-square col-1"></i>
                     <i class="bi bi-x-lg col-1"></i>
@@ -23,36 +23,36 @@
 
 <div class="container px-4 py-5" id="hanging-icons">
     <h2 class="pb-2 border-bottom">Don't let your honeypot logs go to waste no more</h2>
-    <div class="row g-4 py-5 row-cols-1 row-cols-lg-3">
-      <div class="col d-flex align-items-start">
-        <div class="text-dark flex-shrink-0 me-3 fs-2 mb-3">
-            <i class="icon-square bi bi-camera-video-fill"></i>
+    <div class="row g-4 py-3 row-cols-1 row-cols-lg-3">
+        <div class="col d-flex align-items-start">
+            <div class="text-dark flex-shrink-0 me-3 fs-2 mb-3">
+                <i class="icon-square bi bi-camera-video-fill"></i>
+            </div>
+            <div>
+                <h2>Record 'em</h2>
+                <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
+            </div>
         </div>
-        <div>
-          <h2>Record 'em</h2>
-          <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
+        <div class="col d-flex align-items-start">
+            <div class="text-dark flex-shrink-0 me-3 fs-2 mb-3">
+                <i class="icon-square bi bi bi-share-fill"></i>
+            </div>
+            <div>
+                <h2>Share 'em</h2>
+                <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
+            </div>
         </div>
-      </div>
-      <div class="col d-flex align-items-start">
-        <div class="text-dark flex-shrink-0 me-3 fs-2 mb-3">
-            <i class="icon-square bi bi bi-share-fill"></i>
+        <div class="col d-flex align-items-start">
+            <div class="text-dark flex-shrink-0 me-3 fs-2 mb-3">
+                <i class="icon-square bi bi-mortarboard-fill"></i>
+            </div>
+            <div>
+                <h2>Learn from 'em?</h2>
+                <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
+            </div>
         </div>
-        <div>
-          <h2>Share 'em</h2>
-          <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
-        </div>
-      </div>
-      <div class="col d-flex align-items-start">
-        <div class="text-dark flex-shrink-0 me-3 fs-2 mb-3">
-            <i class="icon-square bi bi-mortarboard-fill"></i>
-        </div>
-        <div>
-          <h2>Learn from 'em?</h2>
-          <p>Paragraph of text beneath the heading to explain the heading. We'll add onto it with another sentence and probably just keep going until we run out of words.</p>
-        </div>
-      </div>
     </div>
-  </div>
+</div>
 
 <script src="/js/asciinema-player.min.js"></script>
 <script>
