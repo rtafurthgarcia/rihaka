8dbab958cf227da75f6ab65da154168acedc0346
Richard Tafurth-Garcia <r.tafurthgarcia@hftm.ch>
2022-09-18 06:23:33 +0000

    Nav more responsive

--- app/Views/Base/Header.php
+++ app/Views/Base/Header.php
@@ -1,17 +1,17 @@
 <header class="p-3 text-bg-dark">
-    <nav class="navbar navbar-expand-lg bg-dark navbar-dark">
+    <nav class="navbar navbar-expand-md bg-dark navbar-dark">
       <div class="container">
         <a href="#" class="navbar-brand fs-2">RIHAKA</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navmenu">
-          <span class="navbar-toggler-icon"></span>
+        <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse">
-          <ul class="navbar-nav" id="navmenu">
-            <li class="nav-item"><a class="nav-item nav-link active" href="#" aria-current="page">Home <span class="visually-hidden">(current)</span></a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-white">Categories</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-white">Explore</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-white">Setup</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-white">Community</a></li>
+        <div class="collapse navbar-collapse" id="navmenu">
+          <ul class="navbar-nav">
+            <li class="nav-item"><a class="nav-item nav-link active fs-1-md" href="#" aria-current="page">Home <span class="visually-hidden">(current)</span></a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-white fs-1-md">Categories</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-white fs-1-md">Explore</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-white fs-1-md">Setup</a></li>
+            <li class="nav-item"><a href="#" class="nav-link px-2 text-white fs-1-md">Community</a></li>
           </ul>
           <div class="ms-auto text-end">
             <button type="button" class="btn btn-outline-light me-2">Login</button>
