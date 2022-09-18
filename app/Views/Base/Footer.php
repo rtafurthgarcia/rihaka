cff574b5a5fee0a599b2768999130426454513d9
Richard Tafurth-Garcia <r.tafurthgarcia@hftm.ch>
2022-09-18 09:39:38 +0000

    Links inside the footer

--- app/Views/Base/Footer.php
+++ app/Views/Base/Footer.php
@@ -1,5 +1,7 @@
 <footer class="container d-flex flex-wrap justify-content-between align-items-center py-3 my-6 border-top">
-  <p class="col-12 col-md-6 mb-0 text-muted text-md-start text-center">2022 RIHAKA, a lovely school project from the HFTM, for the HTFM</p>
+  <div class="col-12 col-md-6 mb-0 text-muted text-md-start text-center">
+    2022 RIHAKA, a lovely school project from the <a href="https://www.hftm.ch/de" class="link-warning">HFTM</a>, for the <a href="https://www.hftm.ch/de" class="link-warning">HFTM</a>
+    by <a href="https://github.com/rtafurthgarcia/rihaka" class="link-warning">HFTM students.</a></div>
 
   <ul class="col-12 nav col-md-4 justify-content-center justify-content-md-end">
     <li class="nav-item"><a href="#" class="nav-link px-2 text-muted">Contact</a></li>
