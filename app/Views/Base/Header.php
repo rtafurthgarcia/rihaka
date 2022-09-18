e67f9eea89e2f9410afcb40a68eee8d8c3ca28b2
Richard Tafurth-Garcia <r.tafurthgarcia@hftm.ch>
2022-09-18 09:06:49 +0000

    Nav fully responsive

--- app/Views/Base/Header.php
+++ app/Views/Base/Header.php
@@ -1,23 +1,23 @@
 <header class="p-3 text-bg-dark">
-    <nav class="navbar navbar-expand-md bg-dark navbar-dark">
-      <div class="container">
-        <a href="#" class="navbar-brand fs-2">RIHAKA</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navmenu">
+  <nav class="navbar navbar-expand-md bg-dark navbar-dark">
+    <div class="container">
+      <a href="#" class="navbar-brand fs-2">RIHAKA</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navmenu">
         <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navmenu">
-          <ul class="navbar-nav">
-            <li class="nav-item"><a class="nav-item nav-link active fs-1-md" href="#" aria-current="page">Home <span class="visually-hidden">(current)</span></a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-white fs-1-md">Categories</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-white fs-1-md">Explore</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-white fs-1-md">Setup</a></li>
-            <li class="nav-item"><a href="#" class="nav-link px-2 text-white fs-1-md">Community</a></li>
-          </ul>
-          <div class="ms-auto text-end">
-            <button type="button" class="btn btn-outline-light me-2">Login</button>
-            <button type="button" class="btn btn-warning">Sign up</button>
-          </div>
+      </button>
+      <div class="collapse navbar-collapse row align-items-start mt-3 mt-md-0" id="navmenu">
+        <ul class="navbar-nav col-6">
+          <li class="nav-item"><a class="nav-link active px-2 fs-1-md" href="#" aria-current="page">Home <span class="visually-hidden">(current)</span></a></li>
+          <li class="nav-item"><a href="#" class="nav-link px-2 text-white fs-1-md">Categories</a></li>
+          <li class="nav-item"><a href="#" class="nav-link px-2 text-white fs-1-md">Explore</a></li>
+          <li class="nav-item"><a href="#" class="nav-link px-2 text-white fs-1-md">Setup</a></li>
+          <li class="nav-item"><a href="#" class="nav-link px-2 text-white fs-1-md">Community</a></li>
+        </ul>
+        <div class="ms-auto text-end col-6 gap-2 row justify-content-end">
+          <button type="button" class="btn btn-outline-light col-12 col-md-3">Login</button>
+          <button type="button" class="btn btn-warning col-12 col-md-3">Sign up</button>
         </div>
       </div>
-    </nav>
-  </header>
+    </div>
+  </nav>
+</header>
