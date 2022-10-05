b46c7dbff1b8f0e0d39fc8fdbea542101392d28b
Richard Tafurth-Garcia <r.tafurthgarcia@hftm.ch>
2022-10-05 19:47:39 +0000

    New format for user account page.

--- app/Views/UserAccount.php
+++ app/Views/UserAccount.php
@@ -6,41 +6,35 @@
                 <div class="alert alert-success" role="alert">
                     User information updated successfully!
                 </div>
-                <? endif; ?>
-                <h2>Account informations</h2>
-                <form id="account-form" onsubmit="onFormSubmitted(event)" method="post" class="needs-validation" novalidate>
+            <? endif; ?>
+            <h2>Account informations</h2>
+            <form id="account-form" onsubmit="onFormSubmitted(event)" method="post" class="needs-validation container col-12 col-md-9 text-start p-3 m-0" novalidate>
                 <div class="row">
-                    <div class="row">
-                        <div class="input-group mb-3">
-                            <img src="/images/pp.jpg" class="img-thumbnail col-12 col-md">
-                        </div>
-                        <div class="form-floating mb-3 col">
-                            <textarea class="form-control" placeholder="Write something about ya. Keep in mind everything's gonna be sanitized except links to external websites." id="biography" style="height: 100px"></textarea>
-                            <label class="ms-2 ps-3" for="biography">Biography</label>
+                    <div class="form-floating mb-3 col ps-0 p-md">
+                        <textarea class="form-control" placeholder="Write something about ya. Keep in mind everything's gonna be sanitized except links to external websites." id="biography" style="height: 100%"></textarea>
+                        <label for="biography">Biography</label>
+                    </div>
+                    <img src="/images/pp.jpg" class="mb-3 p-0 col-3 img-thumbnail">
+                </div>
+                <div class="row">
+                    <div class="form-floating mb-3 ps-0 p-md col">
+                        <input type="email" class="form-control <?=(isset($errors["email"])) ? "is-invalid" : "" ?>" name="email" id="floating-email" minlength="3" maxlength="254" placeholder="johndoe@hftm.ch" value="<?=isset($user) ? $user->getemail() : ''?>" autocomplete="email" required>
+                        <label for="floating-email">Email address</label>
+                        <div class="invalid-feedback">
+                            <?=(isset($errors["email"])) ? $errors["email"] : "E-Mail doesnt match a proper E-Mail pattern." ?>
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="form-floating mb-3 col">
-                            <input type="email" class="form-control <?=(isset($errors["email"])) ? "is-invalid" : "" ?>" name="email" id="floating-email" minlength="3" maxlength="254" placeholder="johndoe@hftm.ch" value="<?=isset($user) ? $user->getemail() : ''?>" autocomplete="email" required>
-                            <label for="floating-email" class="ms-2 ps-3">Email address</label>
-                            <div class="invalid-feedback">
-                                <?=(isset($errors["email"])) ? $errors["email"] : "E-Mail doesnt match a proper E-Mail pattern." ?>
-                            </div>
-                        </div>
-                        <div class="input-group mb-3 col">
-                            <span class="input-group-text">@</span>
-                            <div class="form-floating">
-                                <input type="text" class="form-control" name="username" id="floating-user" placeholder="Username" minlength="3" maxlength="30" value="<?=isset($user) ? $user->getuserName() : ''?>" autocomplete="username" readonly>
-                                <label for="floating-user">Username</label>
-                            </div>
+                    <div class="input-group mb-3 col p-0 p-md">
+                        <span class="input-group-text">@</span>
+                        <div class="form-floating">
+                            <input type="text" class="form-control" name="username" id="floating-user" placeholder="Username" minlength="3" maxlength="30" value="<?=isset($user) ? $user->getuserName() : ''?>" autocomplete="username" readonly>
+                            <label for="floating-user">Username</label>
                         </div>
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col">
-                            <button id="submit-button" type="submit" class="btn btn-secondary mb-3">Save</button>
-                    </div>
-                    <div class="col">
+                <div class="row justify-content-between">
+                    <div class="col-12 px-0">
+                        <button id="submit-button" type="submit" class="btn btn-secondary mb-3">Save</button>
                         <a href="/user/<?=$_SESSION['username']?>" class="btn btn-warning float-end" role="button">Cancel</a>
                     </div>
                 </div>
