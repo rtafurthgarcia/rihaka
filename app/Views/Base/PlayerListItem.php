c3c4aedb29baf1f72f388358c1b337eef4aee348
Richard Tafurth-Garcia <r.tafurthgarcia@hftm.ch>
2022-10-11 20:13:44 +0000

    Replace the buttons by a simple click on the recording

--- app/Views/Base/PlayerListItem.php
+++ app/Views/Base/PlayerListItem.php
@@ -1,5 +1,5 @@
-<div class="card shadow-sm col-12 col-lg-4 mx-3 mb-3 px-0">
-    <div class="player-light rounded-top-2">
+<div class="card shadow-sm col-12 col-lg-4 ms-3 me-5 mb-3 px-0">
+    <div class="player-light rounded-top-2" onclick="location.href='<?='/recording/' . $recording->getSecondaryId()?>'">
         <source src="<?='/' . $recording->getVideoLink()?>">
         <time class="d-none"><?=\App\Core\ConverterHelper::secondsToTime($recording->getTimeToDisplay())?></time>
     </div>
@@ -12,11 +12,6 @@
             <p class="card-text"><?=$recording->getTitle()?></p>
         </div>
         <div class="d-flex justify-content-between align-items-center">
-            <? if ($_SESSION['id'] === $recording->getUserId()): ?>
-                <div class="btn-group">
-                    <a href="/recording/<?=$recording->getSecondaryId()?>" class="btn btn-sm btn-outline-secondary" role="button">Edit</a>
-                </div>
-            <? endif; ?>
             <div class="btn-group">
                 <? foreach($recording->getCategories() as $category): ?>
                     <a href="#" class="btn btn-sm btn-outline-secondary" role="button"><?=$category->getName()?></a>
