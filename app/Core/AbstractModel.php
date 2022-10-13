0b91923ba925bc64a8e6a1dbf0ef2345b7d9ddcc
Richard Tafurth-Garcia <111584774+rtafurthgarcia@users.noreply.github.com>
2022-10-13 15:05:54 +0200

    Update comments

--- app/Core/AbstractModel.php
+++ app/Core/AbstractModel.php
@@ -4,15 +4,29 @@ namespace App\Core;
 
 use PDO;
 
+/** 
+ * Parent class for all of our Model classes. 
+ * PDO $_connection will be used to directly query and update tables.
+ * $_tableName will be used as a way to avoid typing manually the table's name everytime.
+ * $_primaryKey is present cause supposedly all records have one. 
+ *
+ * Because all of our models will probably share some insert and update functions, _insert() and _update() have been as abstact as to force their 
+ * implementation. Both delete() and save() are managed directly by the parent for it is universal for (nearly) all records. 
+ * Actually just noticed it didnt apply if u had a pk made of many different fk. 
+ *
+**/
 abstract class AbstractModel {
     protected $_connection;
-
     protected $_tableName;
-    
     protected $_primaryKey;
 
+    /** 
+      * Constructor
+      * @param string $tableName: table linked to our new Model
+      */ 
     public function __construct(string $tableName)
     {
+	// Loads db settings from the env variables
         $password = $_ENV["POSTGRES_PASSWORD"];
         $user = $_ENV["POSTGRES_USER"];
         $db = $_ENV["POSTGRES_DB"];
@@ -27,8 +41,8 @@ abstract class AbstractModel {
     protected abstract function _insert();
     protected abstract function _update();
 
-        /**
-     *
+    /**
+     * Apply modifications to the real linked record on the table. 
      * @return mixed
      */
     public function save() {
@@ -40,8 +54,7 @@ abstract class AbstractModel {
     }
 
     /**
-     *
-     * @return mixed
+     * Delete the real record in the db. Doesnt destroy the object!
      */
     public function delete() {
         $addSupporter = $this->_connection->prepare(
@@ -54,12 +67,14 @@ abstract class AbstractModel {
     }
 
 	/**
-	 * @return mixed
+	 * Returns the pk
+	 * @return pk
 	 */
 	public function getPrimaryKey() {
 		return $this->_primaryKey;
 	}
 	/**
+	 * Sets the pk
 	 * @param mixed $_primaryKey 
 	 * @return AbstractModel
 	 */
